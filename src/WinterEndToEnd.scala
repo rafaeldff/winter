7f91298cc667f5a4a2dfab897b2667e35cb66014
rafaeldff <public@rafaelferreira.net>
2010-05-12 20:17:33 -0300

    Hello world with Jetty

--- src/WinterEndToEnd.scala
+++ src/WinterEndToEnd.scala
@@ -1,8 +1,18 @@
-import org.eclipse.jetty.server.Server;
-
+import java.lang.String
+import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
+import org.eclipse.jetty.server.{Request, Handler, Server}
+import org.eclipse.jetty.server.handler.AbstractHandler
 object WinterEndToEnd {
   def main(args:Array[String]) = {
     val server = new Server(9000)
+    server setHandler new AbstractHandler {
+      def handle(target: String, baseRequest: Request, request: HttpServletRequest, response: HttpServletResponse) = {
+        response.setContentType("text/html;charset=utf-8")
+        response.setStatus(HttpServletResponse.SC_OK)
+        response.getWriter.println("<h1>Hello!</h1>")
+        baseRequest.setHandled(true)
+      }
+    }
     server.start()
     server.join();    
   }
