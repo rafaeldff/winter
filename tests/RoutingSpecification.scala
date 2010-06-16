434349f607e46aa6bb7d6ff44f2f5da73ca82f14
rafaeldff <public@rafaelferreira.net>
2010-06-16 13:33:56 -0300

    Better routing tests

--- tests/RoutingSpecification.scala
+++ tests/RoutingSpecification.scala
@@ -16,22 +16,28 @@ class RoutingSpecification extends Specification {
   }
 
   "Path extractor" should {
-    "match a single path component URI" in {
+    "match a single path component against a single path component URI" in {
       val request = requestForUri("/pathComponent")
       request must beLike {
         case WinterPath("pathComponent") => true
       }
     }
-    "match a multiple path components URI" in {
+    "match multiple path components agaisnt a multiple path component URI" in {
       val request = requestForUri("/first/second")
       request must beLike {
         case WinterPath("first", "second") => true
       }
     }
-    "not match a different URI" in {
+    "not match a path component agaist an URI with a differing path component" in {
       val request = requestForUri("/someOtherThing");
       request must not(beLike {
-        case WinterPath("pathComponent") => true
+        case WinterPath("somePath") => true
+      })
+    }
+    "not match empty pattern agains nonempty URI" in {
+      val request = requestForUri("/something");
+      request must not(beLike {
+        case WinterPath() => true
       })
     }
   }
