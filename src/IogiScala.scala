0cf32f916735c167af77b7ebbb60b7ee7d51784f
rafaeldff <public@rafaelferreira.net>
2010-06-28 23:55:39 -0300

    Files for Iogi integration

---
+++ src/IogiScala.scala
@@ -0,0 +1,3 @@
+package winter.iogi
+
+
---
+++ tests/WinterIogiSpecification.scala
@@ -0,0 +1,11 @@
+package winter
+
+import org.specs.Specification
+
+class WinterIogiSpecification extends Specification {
+  "Winter Iogi integration" should {
+    "instantiate a simple object" in {
+      skip("waiting for integration test")
+    }
+  }
+}
