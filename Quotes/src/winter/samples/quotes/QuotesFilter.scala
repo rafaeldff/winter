0c5b7d415914ab31932d7b05d62fc6707beb8433
rafaeldff <public@rafaelferreira.net>
2010-07-10 19:53:13 -0300

    QuotesServlet changed to QuotesFilter

--- Quotes/src/winter/samples/quotes/QuotesFilter.scala
+++ Quotes/src/winter/samples/quotes/QuotesFilter.scala
@@ -4,7 +4,7 @@ import javax.servlet.http.{HttpServletResponse, HttpServletRequest, HttpServlet}
 import winter.WinterBootstrap
 import winter.samples.Quotes.QuotesApplication
 
-class QuotesServlet extends HttpServlet {
+class QuotesFilter extends HttpServlet {
   override def service(req: HttpServletRequest, resp: HttpServletResponse) = {
     WinterBootstrap.process(QuotesApplication)(req,resp)    
   }
